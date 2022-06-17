3c4661d0265db46f9d4f6bae4ff4e294c8a04c34
brandon-man <nybrandonman@gmail.com>
2022-06-17 13:08:21 -0700

    favicon

--- components/Meta.js
+++ components/Meta.js
@@ -5,13 +5,14 @@ function Meta({
   description = "My personal website built with Next.js and Chakra UI",
 }) {
   return (
-    <>
+    <div>
       <Head>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
+    </div>
   );
 }
 
